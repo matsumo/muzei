94edce1ff35a214bd9be9624d46248eb8ee21dd5
matsumo <matsumo@ce.ns0.it>
2014-03-04 15:45:03 +0900

    uses Stack Blur instead of renderscript-v8.

--- main/src/main/java/com/google/android/apps/muzei/render/ImageBlurrer.java
+++ main/src/main/java/com/google/android/apps/muzei/render/ImageBlurrer.java
@@ -18,10 +18,12 @@ package com.google.android.apps.muzei.render;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.support.v8.renderscript.Allocation;
-import android.support.v8.renderscript.Element;
-import android.support.v8.renderscript.RenderScript;
-import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import android.os.Build;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.util.Log;
 
 public class ImageBlurrer {
     public static final int MAX_SUPPORTED_BLUR_PIXELS = 25;
@@ -32,8 +34,10 @@ public class ImageBlurrer {
     private Allocation mTmpOut;
 
     public ImageBlurrer(Context context) {
-        mRS = RenderScript.create(context);
-        mSIBlur = ScriptIntrinsicBlur.create(mRS, Element.U8_4(mRS));
+        if (Build.VERSION.SDK_INT > 16) {
+            mRS = RenderScript.create(context);
+            mSIBlur = ScriptIntrinsicBlur.create(mRS, Element.U8_4(mRS));
+        }
     }
 
     public Bitmap blurBitmap(Bitmap src, float radius) {
@@ -42,31 +46,263 @@ public class ImageBlurrer {
             return dest;
         }
 
-        if (mTmpIn != null) {
-            mTmpIn.destroy();
-        }
-        if (mTmpOut != null) {
-            mTmpOut.destroy();
+        if (Build.VERSION.SDK_INT > 16) {
+            if (mTmpIn != null) {
+                mTmpIn.destroy();
+            }
+            if (mTmpOut != null) {
+                mTmpOut.destroy();
+            }
+
+            mTmpIn = Allocation.createFromBitmap(mRS, src);
+            mTmpOut = Allocation.createFromBitmap(mRS, dest);
+
+            mSIBlur.setRadius((int) radius);
+            mSIBlur.setInput(mTmpIn);
+            mSIBlur.forEach(mTmpOut);
+            mTmpOut.copyTo(dest);
+            return dest;
         }
 
-        mTmpIn = Allocation.createFromBitmap(mRS, src);
-        mTmpOut = Allocation.createFromBitmap(mRS, dest);
+        int radius_ = (int)radius;
 
-        mSIBlur.setRadius((int) radius);
-        mSIBlur.setInput(mTmpIn);
-        mSIBlur.forEach(mTmpOut);
-        mTmpOut.copyTo(dest);
-        return dest;
+        // Stack Blur v1.0 from
+        // http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
+        //
+        // Java Author: Mario Klingemann <mario at quasimondo.com>
+        // http://incubator.quasimondo.com
+        // created Feburary 29, 2004
+        // Android port : Yahel Bouaziz <yahel at kayenko.com>
+        // http://www.kayenko.com
+        // ported april 5th, 2012
+
+        // This is a compromise between Gaussian Blur and Box blur
+        // It creates much better looking blurs than Box Blur, but is
+        // 7x faster than my Gaussian Blur implementation.
+        //
+        // I called it Stack Blur because this describes best how this
+        // filter works internally: it creates a kind of moving stack
+        // of colors whilst scanning through the image. Thereby it
+        // just has to add one new block of color to the right side
+        // of the stack and remove the leftmost color. The remaining
+        // colors on the topmost layer of the stack are either added on
+        // or reduced by one, depending on if they are on the right or
+        // on the left side of the stack.
+        //
+        // If you are using this algorithm in your code please add
+        // the following line:
+        //
+        // Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
+
+        if (radius_ < 1) {
+            return (dest);
+        }
+
+        int w = dest.getWidth();
+        int h = dest.getHeight();
+
+        int[] pix = new int[w * h];
+//        Log.e("pix", w + " " + h + " " + pix.length);
+        dest.getPixels(pix, 0, w, 0, 0, w, h);
+
+        int wm = w - 1;
+        int hm = h - 1;
+        int wh = w * h;
+        int div = radius_ + radius_ + 1;
+
+        int r[] = new int[wh];
+        int g[] = new int[wh];
+        int b[] = new int[wh];
+        int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
+        int vmin[] = new int[Math.max(w, h)];
+
+        int divsum = (div + 1) >> 1;
+        divsum *= divsum;
+        int dv[] = new int[256 * divsum];
+        for (i = 0; i < 256 * divsum; i++) {
+            dv[i] = (i / divsum);
+        }
+
+        yw = yi = 0;
+
+        int[][] stack = new int[div][3];
+        int stackpointer;
+        int stackstart;
+        int[] sir;
+        int rbs;
+        int r1 = radius_ + 1;
+        int routsum, goutsum, boutsum;
+        int rinsum, ginsum, binsum;
+
+        for (y = 0; y < h; y++) {
+            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+            for (i = -radius_; i <= radius_; i++) {
+                p = pix[yi + Math.min(wm, Math.max(i, 0))];
+                sir = stack[i + radius_];
+                sir[0] = (p & 0xff0000) >> 16;
+                sir[1] = (p & 0x00ff00) >> 8;
+                sir[2] = (p & 0x0000ff);
+                rbs = r1 - Math.abs(i);
+                rsum += sir[0] * rbs;
+                gsum += sir[1] * rbs;
+                bsum += sir[2] * rbs;
+                if (i > 0) {
+                    rinsum += sir[0];
+                    ginsum += sir[1];
+                    binsum += sir[2];
+                } else {
+                    routsum += sir[0];
+                    goutsum += sir[1];
+                    boutsum += sir[2];
+                }
+            }
+            stackpointer = radius_;
+
+            for (x = 0; x < w; x++) {
+
+                r[yi] = dv[rsum];
+                g[yi] = dv[gsum];
+                b[yi] = dv[bsum];
+
+                rsum -= routsum;
+                gsum -= goutsum;
+                bsum -= boutsum;
+
+                stackstart = stackpointer - radius_ + div;
+                sir = stack[stackstart % div];
+
+                routsum -= sir[0];
+                goutsum -= sir[1];
+                boutsum -= sir[2];
+
+                if (y == 0) {
+                    vmin[x] = Math.min(x + radius_ + 1, wm);
+                }
+                p = pix[yw + vmin[x]];
+
+                sir[0] = (p & 0xff0000) >> 16;
+                sir[1] = (p & 0x00ff00) >> 8;
+                sir[2] = (p & 0x0000ff);
+
+                rinsum += sir[0];
+                ginsum += sir[1];
+                binsum += sir[2];
+
+                rsum += rinsum;
+                gsum += ginsum;
+                bsum += binsum;
+
+                stackpointer = (stackpointer + 1) % div;
+                sir = stack[(stackpointer) % div];
+
+                routsum += sir[0];
+                goutsum += sir[1];
+                boutsum += sir[2];
+
+                rinsum -= sir[0];
+                ginsum -= sir[1];
+                binsum -= sir[2];
+
+                yi++;
+            }
+            yw += w;
+        }
+        for (x = 0; x < w; x++) {
+            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+            yp = -radius_ * w;
+            for (i = -radius_; i <= radius_; i++) {
+                yi = Math.max(0, yp) + x;
+
+                sir = stack[i + radius_];
+
+                sir[0] = r[yi];
+                sir[1] = g[yi];
+                sir[2] = b[yi];
+
+                rbs = r1 - Math.abs(i);
+
+                rsum += r[yi] * rbs;
+                gsum += g[yi] * rbs;
+                bsum += b[yi] * rbs;
+
+                if (i > 0) {
+                    rinsum += sir[0];
+                    ginsum += sir[1];
+                    binsum += sir[2];
+                } else {
+                    routsum += sir[0];
+                    goutsum += sir[1];
+                    boutsum += sir[2];
+                }
+
+                if (i < hm) {
+                    yp += w;
+                }
+            }
+            yi = x;
+            stackpointer = radius_;
+            for (y = 0; y < h; y++) {
+                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
+                pix[yi] = (0xff000000 & pix[yi]) | (dv[rsum] << 16) | (dv[gsum] << 8) | dv[bsum];
+
+                rsum -= routsum;
+                gsum -= goutsum;
+                bsum -= boutsum;
+
+                stackstart = stackpointer - radius_ + div;
+                sir = stack[stackstart % div];
+
+                routsum -= sir[0];
+                goutsum -= sir[1];
+                boutsum -= sir[2];
+
+                if (x == 0) {
+                    vmin[y] = Math.min(y + r1, hm) * w;
+                }
+                p = x + vmin[y];
+
+                sir[0] = r[p];
+                sir[1] = g[p];
+                sir[2] = b[p];
+
+                rinsum += sir[0];
+                ginsum += sir[1];
+                binsum += sir[2];
+
+                rsum += rinsum;
+                gsum += ginsum;
+                bsum += binsum;
+
+                stackpointer = (stackpointer + 1) % div;
+                sir = stack[stackpointer];
+
+                routsum += sir[0];
+                goutsum += sir[1];
+                boutsum += sir[2];
+
+                rinsum -= sir[0];
+                ginsum -= sir[1];
+                binsum -= sir[2];
+
+                yi += w;
+            }
+        }
+
+//        Log.e("pix", w + " " + h + " " + pix.length);
+        dest.setPixels(pix, 0, w, 0, 0, w, h);
+        return (dest);
     }
 
     public void destroy() {
-        mSIBlur.destroy();
-        if (mTmpIn != null) {
-            mTmpIn.destroy();
+        if (Build.VERSION.SDK_INT > 16) {
+            mSIBlur.destroy();
+            if (mTmpIn != null) {
+                mTmpIn.destroy();
+            }
+            if (mTmpOut != null) {
+                mTmpOut.destroy();
+            }
+            mRS.destroy();
         }
-        if (mTmpOut != null) {
-            mTmpOut.destroy();
-        }
-        mRS.destroy();
     }
 }
